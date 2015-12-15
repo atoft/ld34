e4e40c31f9ad32d7a4e0b17f84250cacdad1cb4b
alastair <addyfilm@yahoo.com>
2015-12-15 00:44:51 +0000

    Added sound to asteroids

--- js/Asteroid.js
+++ js/Asteroid.js
@@ -26,7 +26,7 @@ Asteroid.prototype.update = function() {
   if(this.health <= 0) {
     console.log("!!!!!!!asteroid destroyed");
     entities.remove(this);
-    
+    if(!mute) audioAsteroid.play();
     //Create debris from the explosion
     if(this.type!=0) { //if not debris
       for(i=0; i<3; i++) {
